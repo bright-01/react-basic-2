5b4176103180d4e7474601ef59a5c9be20322924
mwchoen <cheonmyeongwon@gmail.com>
2023-07-29 16:11:46 +0900

    기본

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import AppXY from './AppXY';
 import AppMentors from "./AppMentors";
 import AppTheme from "./AppTheme";
+import AppProducts from "./AppProducts";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppTheme/>
+    <AppProducts />
   </React.StrictMode>
 );
 
